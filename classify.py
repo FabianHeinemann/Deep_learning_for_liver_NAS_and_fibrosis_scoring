9eaa02e12110d9256e111bf8f0aeaf94fe2752ad
Fabian Heinemann <fabian.heinemann@gmx.net>
2020-01-05 22:08:56 +0100

    Update classify.py

--- classify.py
+++ classify.py
@@ -1,5 +1,5 @@
 """ 
-Copyright (c) 2019, Fabian Heinemann, Gerald Birk, Birgit Stierstorfer; Boehringer Ingelheim Boehringer Ingelheim Pharma GmbH & Co KG
+Copyright (c) 2019, Fabian Heinemann, Gerald Birk, Birgit Stierstorfer; Boehringer Ingelheim Pharma GmbH & Co KG
 All rights reserved.
  
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
